23b8ebabf3d9a16c6e58c554300cdb81cf9b0100
kingjack05 <kingjack05@gmail.com>
2022-05-03 14:22:19 +0800

    time input

--- src/components/UI/Forms/ImageForm/index.jsx
+++ src/components/UI/Forms/ImageForm/index.jsx
@@ -1,7 +1,9 @@
 import { AutosuggestComboBox } from "../../../AutosuggestComboBox"
 
-import { useForm } from "react-hook-form"
-import { Box, Button, Textarea, Flex } from "@chakra-ui/react"
+import { Controller, useForm } from "react-hook-form"
+import { Box, Button, Textarea, Flex, Input } from "@chakra-ui/react"
+import { DateTime } from "luxon"
+import { Calendar } from "@carbon/icons-react"
 
 export const ImageForm = ({ defaultValues, onSubmit, onCancel }) => {
 	const {
@@ -9,6 +11,7 @@ export const ImageForm = ({ defaultValues, onSubmit, onCancel }) => {
 		handleSubmit,
 		formState: { errors },
 		reset,
+		control,
 	} = useForm({
 		defaultValues,
 	})
@@ -29,6 +32,13 @@ export const ImageForm = ({ defaultValues, onSubmit, onCancel }) => {
 					<Flex alignItems="center" mb="1">
 						<Textarea {...register("summary")} placeholder="Summary" variant="carbon" />
 					</Flex>
+					<Controller
+						control={control}
+						name="from"
+						render={({ field: { value, onChange } }) => (
+							<TimeDateInput value={value} onChange={onChange} />
+						)}
+					/>
 				</form>
 			</Box>
 			<Flex>
@@ -47,5 +57,69 @@ export const ImageForm = ({ defaultValues, onSubmit, onCancel }) => {
 				</Button>
 			</Flex>
 		</>
+	)
+}
+
+const TimeDateInput = ({ value = new Date().toISOString(), onChange = () => {} }) => {
+	const dt = DateTime.fromISO(value)
+
+	return (
+		<Flex alignItems="center">
+			<Input
+				value={dt.hour}
+				type="number"
+				onChange={(e) => {
+					const hour = e.target.value
+						? parseInt(e.target.value) >= 24
+							? 23
+							: parseInt(e.target.value)
+						: 0
+					onChange(dt.set({ hour }).toISO())
+				}}
+			/>
+			:
+			<Input
+				value={dt.minute}
+				onChange={(e) => {
+					const minute = e.target.value
+						? parseInt(e.target.value) >= 60
+							? 59
+							: parseInt(e.target.value)
+						: 0
+					onChange(dt.set({ minute }).toISO())
+				}}
+				mr="2"
+			/>
+			{dt.month}/{dt.day}/{dt.year}
+			<Box position="relative" flexShrink="0" ml="2">
+				<Calendar size="20" />
+				<span
+					style={{
+						position: "absolute",
+						left: "0",
+						top: "0",
+						width: "100%",
+						height: "100%",
+					}}
+				></span>
+				<input
+					type="date"
+					style={{
+						position: "absolute",
+						left: "0",
+						top: "0",
+						width: "100%",
+						height: "100%",
+						opacity: "0",
+						cursor: "pointer",
+						boxSizing: "border-box",
+					}}
+					onChange={(e) => {
+						const [year, month, day] = e.target.value.split("-")
+						onChange(dt.set({ year, month, day }).toISO())
+					}}
+				/>
+			</Box>
+		</Flex>
 	)
 }
